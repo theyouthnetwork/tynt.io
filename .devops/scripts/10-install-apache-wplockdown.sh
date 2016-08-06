a78eca8612d225edd8ab811d46d6cbd64331df31
Alan Yeung <alan@creativebits.com.au>
2016-08-06 09:44:07 -0400

    Reordered devops scripts
    Now it feels more like a classic post-install system

---
+++ .devops/scripts/10-install-apache-wplockdown.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DIRNAME=`dirname $0`
+WPLOCKDOWN_FILE='tynt-wordpress-harden.conf'
+WPLOCKDOWN_FILE_PATH="$DIRNAME/../conf/$WPLOCKDOWN_FILE"
+
+## Expect debian-like apache 
+APACHE_CONF='/etc/apache2/'
+APACHE_CONF_DROPIN_DIRNAME='conf-available'
+APACHE_CONF_ENABLED_DIRNAME='conf-enabled'
+
+sudo cp "$WPLOCKDOWN_FILE_PATH" "$APACHE_CONF/$APACHE_CONF_DROPIN_DIRNAME"
+
+## Symlink it to conf-enabled to make it work
+cd "$APACHE_CONF/$APACHE_CONF_ENABLED_DIRNAME"
+ln -s "../$APACHE_CONF_DROPIN_DIRNAME" "$WPLOCKDOWN_FILE"
---
+++ .devops/scripts/30-install-github-listener.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+## Expecting the linux box to not have drop of nodejs on it
+
+sudo apt-get install node npm
+sudo npm install pm2 -g
+
